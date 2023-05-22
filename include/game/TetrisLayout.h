92eceefeef5b7563c586a27a3df69efa95c870e7
Ma3yTuK <nazarmatveev2003@gmail.com>
2023-05-22 18:40:51 +0300

    Codestyle changes

--- include/game/TetrisLayout.h
+++ include/game/TetrisLayout.h
@@ -4,16 +4,18 @@
 class TetrisLayout
 {
 private:
-    int width;
     int height;
+    int width;
     int** layout;
 
 public:
-    TetrisLayout(int width, int height);
+    TetrisLayout(int height, int width);
+    TetrisLayout(const TetrisLayout& obj);
     int getWidth() const { return width; }
     int getHeight() const { return height; }
     TetrisLayout operator|(const TetrisLayout& secondLayout);
     int& operator()(int x, int y);
+    ~TetrisLayout();
 };
 
 #endif // TETRISLAYOUT_H
