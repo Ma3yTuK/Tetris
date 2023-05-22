50a5c961bb0326f762572b9fe5659233fb54008b
Ma3yTuK <nazarmatveev2003@gmail.com>
2023-05-22 18:23:56 +0300

    TetrisLayout class implemented

--- include/game/TetrisLayout.h
+++ include/game/TetrisLayout.h
@@ -1,6 +1,19 @@
 #ifndef TETRISLAYOUT_H
 #define TETRISLAYOUT_H
 
-#include <QColor>
+class TetrisLayout
+{
+private:
+    int width;
+    int height;
+    int** layout;
+
+public:
+    TetrisLayout(int width, int height);
+    int getWidth() const { return width; }
+    int getHeight() const { return height; }
+    TetrisLayout operator|(const TetrisLayout& secondLayout);
+    int& operator()(int x, int y);
+};
 
 #endif // TETRISLAYOUT_H
